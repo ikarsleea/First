159a79eff55748523ad148c32ae4f31fb2d9cd78
Valentina Toloza Gutiérrez <valentinatoloza.est@gmail.com>
2024-09-07 10:56:53 -0400

    first commit

---
+++ Probando.py
@@ -0,0 +1,14 @@
+print("hola mundo como estaaan")
+print("quiero aprender a programar")
+print("aunque cueste")
+# soy un comentario
+# aber esta opción de poner comentariooos
+"""
+aló?
+muy buenas tardeeees
+"""
+# definir variables
+name = "Valentina Toloza"
+age = "19"
+print(age)
+print(f"{age} cualquier cosa {name}")
